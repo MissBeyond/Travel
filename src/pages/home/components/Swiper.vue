184b5af6c5e5b906c70ab7b1e87486c5b17602f5
Beyond <2546776177@qq.com>
2018-06-01 00:03:11 +0800

    add

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -6,9 +6,8 @@
                 <img class="swiper-img" :src="item.imgUrl"/>
             </swiper-slide>
             <div class="swiper-pagination"  slot="pagination"></div>
-        
-         </swiper>
-  </div>
+        </swiper>
+    </div>
 </template>
 <script>
 export default {
@@ -16,35 +15,46 @@ export default {
   data() {
     return {
       swiperOption: {
-          pagination: '.swiper-pagination',
-          loop: true
+        pagination: ".swiper-pagination",
+        loop: true
       },
-      swiperList: [{
-          id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
-          },{
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
-          },{
-          id: '0003',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/85/ed01cd5307b3f602.jpg_750x200_e54ef754.jpg'
-          }]
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1805/85/ed01cd5307b3f602.jpg_750x200_e54ef754.jpg"
+        }
+      ]
     };
   }
 };
 </script>
 <style lang="stylus" scoped>
-.wrapper >>> .swiper-pagination-bullet-active
-    background-color #fff
-.wrapper
-    overflow hidden
-    width 100%
-    height 
-    padding-bottom 31.25%
-    background-color #eee
-    .swiper-img 
-        width 100%
-        
+.wrapper >>> .swiper-pagination-bullet-active {
+    background-color: #fff;
+}
+
+.wrapper {
+    overflow: hidden;
+    width: 100%;
+    height;
+    padding-bottom: 31.25%;
+    background-color: #eee;
+
+    .swiper-img {
+        width: 100%;
+    }
+}
 </style>
 
 
